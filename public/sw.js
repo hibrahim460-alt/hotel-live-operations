b02c46309b2b4fac03ed4ccfed83078f620c061c
hibrahim460-alt <hibrahim460@gmail.com>
2026-06-07 21:22:19 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'wh-core-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
